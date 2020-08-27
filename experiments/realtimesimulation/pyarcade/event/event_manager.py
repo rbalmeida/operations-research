75f030af38dde79455a2e172fd14b37a1ad24332
ralmeida <rafael.b.almeida@gmail.com>
2020-08-26 21:39:05 -0300

    Simple Event Manager Test

---
+++ experiments/realtimesimulation/pyarcade/event/event_manager.py
@@ -0,0 +1,35 @@
+class EventListener:
+    def notify(self, event):
+        print("event")
+        print(event)
+
+class EventManager:
+
+    events = []
+    listeners = []
+
+
+    def raise_event(self, event):
+        self.events.append(event)
+
+    def subscribe(self, event_listener):
+        self.listeners.append(event_listener)
+
+    def run(self):
+        for event in self.events:
+            for listener in self.listeners:
+                listener.notify(event)
+
+
+event_manager = EventManager()
+
+event_listener = EventListener()
+event_manager.subscribe(event_listener)
+
+event_listener2 = EventListener()
+event_manager.subscribe(event_listener2)
+
+event_manager.raise_event({"siren"})
+event_manager.raise_event(({"noise"}))
+
+event_manager.run()
